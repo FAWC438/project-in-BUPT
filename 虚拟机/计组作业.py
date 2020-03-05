99360a7463398eb73f4cbdb18ad9ad35fd365c6d
FAWC-bupt <43695833+FAWC-bupt@users.noreply.github.com>
2020-03-05 21:42:14 +0800

    Update 计组作业.py

--- 虚拟机/计组作业.py
+++ 虚拟机/计组作业.py
@@ -61,7 +61,8 @@ if __name__ == '__main__':
             logger.info(
                 '-----------------------------------------------------------')
             logger.info('正在调试模式中，现在执行的PC地址（行数）是：' + str(PC))
-            logger.info('当前参数值    AR:' + str(AR) + '    AC:' + str(AC) + '    DR:' + DR + '    IR:' + IR)
+            logger.info('当前寄存器值\nAR:    ' + str(AR) + '\nAC:    ' +
+                        str(AC) + '\nDR:    ' + DR + '\nIR:    ' + IR + '\nPSW:   ' + PSW)
             logger.info('输入q键并按回车键退出，输入其它按键并按回车键继续：')
             flag = input()
             if flag == 'q'or flag == 'Q':
@@ -79,10 +80,14 @@ if __name__ == '__main__':
 
         # 取指流程 T1
         logger.info('已进入第' + str(PC) + '行指令的取指流程')
-        AR = PC  # 计数器地址存入地址寄存器
-        PC += 1  # 计数器加一
-        PSW = 0  # 默认状态
-        DR = data[AR]  # 每次读取一条指令
+        AR = PC
+        logger.info('计数器地址存入地址寄存器')
+        PC += 1
+        logger.info('计数器加一')
+        if PSW == '':
+            PSW = '#'  # 默认状态
+        DR = data[AR]
+        logger.info('读取指令' + DR + '到DR')
         if DR == '\n' or DR == '' or (ord(DR[0]) >= ord('0') and ord(DR[0]) <= ord('9')):
             # 如果遇到立即数而非指令，或是内存中这个地址没有指令
             logger.info('指令文本第' + str(PC - 1) + '行不是一个指令！程序退出...')
@@ -93,10 +98,12 @@ if __name__ == '__main__':
             # 读取某些带目标地址的指令
             temp = DR.split()
             AR = int(temp[1])
+            logger.info('地址寄存器AR更新为：' + str(AR))
             IR = temp[0]
         else:
             # 不带目标地址的指令
             IR = DR
+        logger.info('IR更新为：' + IR)
 
         # 执行流程 T2
         logger.info('已进入第' + str(PC - 1) + '行指令的执行流程')
@@ -104,6 +111,8 @@ if __name__ == '__main__':
             # 清零
             logger.info('执行第' + str(PC - 1) + '行的CLA指令')
             AC = 0
+            PSW = 'Z'
+            logger.info('状态寄存器改变，值为：' + PSW)
         elif IR == 'ADD':
             # 累加
             logger.info('执行第' + str(PC - 1) + '行的ADD指令')
@@ -112,16 +121,18 @@ if __name__ == '__main__':
                 logger.info('第' + str(PC - 1) + '行的ADD指令目标地址错误！程序退出...')
                 time.sleep(2)
                 break
+            logger.info('从第' + str(AR) + '块地址中取出数据' + DR + '存入DR')
             AC += int(DR)
             if AC > 999999:
                 # 溢出
-                AC = AC % 1000000
+                AC = AC % 999999
                 PSW = 'V'
                 logger.info('状态寄存器改变，值为：' + PSW)
             elif AC == 0:
                 # 为零
                 PSW = 'Z'
                 logger.info('状态寄存器改变，值为：' + PSW)
+            logger.info('AC改变为：' + str(AC))
         elif IR == 'STA':
             # 存入
             logger.info('执行第' + str(PC - 1) + '行的STA指令')
@@ -130,6 +141,8 @@ if __name__ == '__main__':
             # 跳转指令容易发生死循环，设置exceptionFlag，当循环超过一定次数时将退出程序
             if AR <= PC:
                 exceptionFlag += 1
+                logger.info('已触发死循环保护机制，重复循环10次时将退出，当前已循环' +
+                            str(exceptionFlag)+'次')
             else:
                 exceptionFlag = 0
             # 跳转
@@ -158,5 +171,7 @@ if __name__ == '__main__':
     for i in range(RANGE):
         logger.info('第' + str(i) + '行：' + data[i])
     logger.info('------------以上是所有内存的情况------------')
+    logger.info('当前寄存器值\nAR:    ' + str(AR) + '\nAC:    ' +
+                str(AC) + '\nDR:    ' + DR + '\nIR:    ' + IR + '\nPSW:   ' + PSW)
     logger.info('程序结束，请到目录下log.txt文件查看运行日志')
     os.system('pause')
